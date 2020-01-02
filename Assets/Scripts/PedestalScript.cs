7cf580fa68f15e8c0700678b464cb232cc03cb25
Robert Lindquist <dtrush@snet.net>
2020-01-02 14:49:02 -0500

    Started adding puzzle parts, working on signalling a movable door

--- Assets/Scripts/PedestalScript.cs
+++ Assets/Scripts/PedestalScript.cs
@@ -6,16 +6,36 @@ public class PedestalScript : MonoBehaviour
 {
 
     [Tooltip("The orb detection trigger on this pedestal.")] public Collider orbDetect;
+    [Tooltip("The list of puzzle objects that this pedestal activates")] public List<PuzzlePartScript> puzzleParts;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (puzzleParts.Count == 0)
+        {
+            Debug.Log("No puzzle parts!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Orb"))
+        {
+            //Debug.Log("Orb!");
+            foreach (PuzzlePartScript activated in puzzleParts)
+            {
+                activated.Activate(other.name);
+            }
+        }
+        else
+        {
+            //Debug.Log("Not orb!");
+        }
     }
 }
---
+++ Assets/Scripts/PuzzlePartScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePartScript : MonoBehaviour
+{
+    [Tooltip("List of animations that this puzzlePart can do, based on what color orb is used")] public Dictionary<string, Animation> actions = new Dictionary<string, Animation>(5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate(string OrbColor)
+    {
+        if (actions.ContainsKey(OrbColor))
+        {
+            //Play animation with key OrbColor from dict actions
+            Debug.Log("playAnimation" + OrbColor);
+        }
+    }
+}
